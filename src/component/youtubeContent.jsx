e99e10643fef49c6f09ce285c3ded994fef2fa41
salangdung-i <dladmswl92@naver.com>
2021-12-08 13:42:36 +0900

    content jsx

--- src/component/youtubeContent.jsx
+++ src/component/youtubeContent.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class YoutubeContent extends Component {
+
   render() {
     return (
-      <div>
-        컨텐츠입니다.
+      <div className="container">
+        <div className="video-list">
+          비디오리스트 영역
+          <ul>
+            <li>
+              <img src="" alt="썸네일이미지" />
+              <span>썸네일 설명</span>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
