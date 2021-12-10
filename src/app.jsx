f390b376fe3b999503da84cbf4779c1639e02711
salangdung-i <dladmswl92@naver.com>
2021-12-10 14:54:02 +0900

    return list

--- src/app.jsx
+++ src/app.jsx
@@ -3,24 +3,43 @@ import React, { useEffect, useState } from 'react';
 import SearchHeader from './component/search_header/search_header';
 import VideoList from './component/video_list/video_list';
 import styles from './app.module.css';
+import VideoDetail from './component/vieo_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
+
   const search = query => {
-    youtube.search(query)
+    setSelectedVideo(null);
+    youtube
+      .search(query)
       .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    youtube.mostPopular()
+    youtube
+      .mostPopular()
       .then(videos => setVideos(videos));
   }, []);
 
   return (
     <div className={styles.app}>
-      <SearchHeader
-        onSearch={search} />
-      <VideoList videos={videos} />
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            {selectedVideo && <VideoDetail video={selectedVideo} />}
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
+        </div>
+
+      </section>
     </div>
   );
 }
