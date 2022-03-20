3efd53dcb25b84840083f6fcd7ac141b7e7c0481
salangdung-i <dladmswl92@naver.com>
2022-03-20 16:15:44 +0900

    프로젝트 초기화

--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,8 @@
+import React from 'react';
 
-import React, { useCallback, useEffect, useState } from 'react';
-import SearchHeader from './component/search_header/search_header';
-import VideoList from './component/video_list/video_list';
-import styles from './app.module.css';
-import VideoDetail from './component/vieo_detail/video_detail';
-
-function App({ youtube }) {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
-
-  const selectVideo = (video) => {
-    setSelectedVideo(video);
-  };
-
-  const search = useCallback(query => {
-    setSelectedVideo(null);
-    youtube
-      .search(query)
-      .then(videos => setVideos(videos));
-  }, []);
-
-  useEffect(() => {
-    youtube
-      .mostPopular()
-      .then(videos => setVideos(videos));
-  }, [youtube]);
-
+const App = () => {
   return (
-    <div className={styles.app}>
-      <SearchHeader onSearch={search} />
-      <section className={styles.content}>
-        {selectedVideo && (
-          <div className={styles.detail}>
-            {selectedVideo && <VideoDetail video={selectedVideo} />}
-          </div>
-        )}
-        <div className={styles.list}>
-          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
-        </div>
-
-      </section>
-    </div>
+    <h1>App</h1>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import Youtube from './service/youtube-fetch copy';
-import axios from 'axios';
 
-
-const httpClient = axios.create({
-  baseURL: 'https://youtube.googleapis.com/youtube/v3',
-  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
-
-});
-const youtube = new Youtube(httpClient);
 ReactDOM.render(
   <React.StrictMode>
-    <App youtube={youtube} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
