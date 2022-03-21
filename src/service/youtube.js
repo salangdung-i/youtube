02bcd79aa2bf9c20af54a77da0a9cded2452fb69
salangdung-i <dladmswl92@naver.com>
2022-03-21 15:53:20 +0900

    youtube API 서비스로직

---
+++ src/service/youtube.js
@@ -0,0 +1,42 @@
+class YoutubeFetch {
+  constructor() {
+    this.key = process.env.REACT_APP_YOUTUBE_API_KEY;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+  }
+
+  async mostPopular() {
+    let response;
+    response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+      this.getRequestOptions
+    );
+
+    if (response.status === 403) {
+      response = await fetch(process.env.PUBLIC_URL + '/data/mostPopular.json').then(response => response.json());
+    }
+
+    console.log(response.items);
+    return response.items;
+
+  };
+
+  async search(query) {
+    let response;
+    response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      this.getRequestOptions
+    );
+    // const result = await response.json();
+
+    if (response.status === 403) {
+      console.log('403');
+      response = await fetch(process.env.PUBLIC_URL + '/data/searchData.json').then(response => response.json());
+    }
+
+    return response.items;
+  };
+}
+
+export default YoutubeFetch;
+
