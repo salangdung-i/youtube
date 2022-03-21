43fe80c320a6b8d8d80143e266f8c7021651e667
salangdung-i <dladmswl92@naver.com>
2022-03-21 15:55:52 +0900

    youtube 컴포넌트 연결, 의존성주입

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import styles from './app.module.css';
+import SearchHeader from './component/search_header/search_header';
+import VideoList from './component/video_list/video_list';
+import VideoDetail from './component/video_detail/video_detail';
 
-const App = () => {
+
+const App = ({ youtubeFetch }) => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
+  const onSearch = useCallback(query => {
+    setSelectedVideo(null);
+    youtubeFetch
+      .search(query)
+      .then(videos => setVideos(videos));
+  }, []);
+
+
+  useEffect(() => {
+    youtubeFetch
+      .mostPopular()
+      .then(videos => setVideos(videos));
+  }, [youtubeFetch]);
+
   return (
-    <h1>App</h1>
+    <div className={styles.app}>
+      <SearchHeader onSearch={onSearch} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.datail}>
+            {selectedVideo && <VideoDetail video={selectedVideo} />}
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
+        </div>
+      </section>
+    </div>
   );
 }
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import YoutubeFetch from './service/youtube';
 
+
+
+const youtubeFetch = new YoutubeFetch();
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtubeFetch={youtubeFetch} />
   </React.StrictMode>,
   document.getElementById('root')
 );
